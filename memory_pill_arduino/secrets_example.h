37a7b10afdbff9271f4131101280eb1ce45929d9
Nick Bild <nick.bild@gmail.com>
2021-07-17 19:30:51 -0400

    Add example secrets file.

---
+++ memory_pill_arduino/secrets_example.h
@@ -0,0 +1,10 @@
+/*
+* Rename this file to "secrets.h", then supply the
+* correct values for the fields below.
+*/
+
+#define SSID "wifi_ssid_here"
+#define PASSWORD "wifi_password"
+#define PATIENT_ID "ID of patient"
+#define MEDICATION_ID "ID of medication"
+
